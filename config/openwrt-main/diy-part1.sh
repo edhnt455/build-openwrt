fdcaa065db6c0fab87cc3e9d25af05aaed8ee7be
edhnt455 <59108157+edhnt455@users.noreply.github.com>
2024-10-02 12:17:22 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #========================================================================================================================
 # https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
+# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/openwrt/openwrt / Branch: main
+# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
 #========================================================================================================================
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # other
-# rm -rf package/utils/{ucode,fbtest}
+# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
